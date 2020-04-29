8353f04f533882f15b3091b3b7218d5829d52fb2
fata <nfatah41@gmail.com>
2020-04-29 09:08:44 +0300

    update linting configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,43 +2,59 @@
 
 module.exports = {
   globals: {
-    "$": true,
-    "jQuery": true,
+    $: true,
+    jQuery: true,
   },
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module'
+    sourceType: 'module',
+    parser: 'babel-eslint',
   },
   env: {
     browser: true,
   },
-  extends: 'airbnb-base',
-  // required to lint *.vue files
-  plugins: [
-    'html'
+  extends: [
+    'eslint:recommended',
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended',
   ],
+
+  // required to lint *.vue files
+  plugins: ['vue'],
   // check if imports actually resolve
-  'settings': {
+  settings: {
     'import/resolver': {
-      'webpack': {
-        'config': 'build/webpack.base.conf.js'
-      }
-    }
+      webpack: {
+        config: 'build/webpack.base.conf.js',
+      },
+    },
   },
   // add your custom rules here
-  'rules': {
+  rules: {
+    semi: [2, 'never'],
+    indent: ['error', 2],
+    'vue/max-attributes-per-line': 'off',
+    'no-console': 'off',
     // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
-    }],
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        vue: 'never',
+      },
+    ],
     // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        optionalDependencies: ['test/unit/index.js'],
+      },
+    ],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'linebreak-style': 0,
-  }
+  },
 }
